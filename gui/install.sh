a3291a28c2e700a2020fe937d810cf3b6e665a47
Collin McKinney <collinmckinney4@gmail.com>
2026-04-24 10:34:20 -0500

    Use sudo to enable bluetooth & NetworkManager.

--- gui/install.sh
+++ gui/install.sh
@@ -60,8 +60,8 @@ apt install --no-install-recommends -y \
 
 mkdir -p /etc/NetworkManager/conf.d
 printf "[main]\nmanaged=true\n" > /etc/NetworkManager/conf.d/10-globally-managed-devices.conf
-systemctl enable bluetooth
-systemctl enable NetworkManager
+sudo systemctl enable bluetooth
+sudo systemctl enable NetworkManager
 
 # Plasma desktop layout.
 mkdir -p "${USER_HOME}/.config"
